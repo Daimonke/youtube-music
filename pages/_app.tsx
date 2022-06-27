b5344f46d8f91d59a96ecbd977c8dfeec56a0b46
Daimonke <Daimonas98@gmail.com>
2022-06-27 20:30:56 +0300

    Fixed: PWA theme color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,7 +30,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           sizes="32x32"
         />
         <link rel="apple-touch-icon" href="./icons/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <meta name="theme-color" content="#2C5364" />
       </Head>
       <Component {...pageProps} />
     </>
