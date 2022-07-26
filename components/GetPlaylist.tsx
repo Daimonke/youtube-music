ba040b0a0a49f264d53ed5fa5d4ea5eae3d90900
Daimonke <Daimonas98@gmail.com>
2022-07-26 18:25:29 +0300

    fixed: player stops on unmount

--- components/GetPlaylist.tsx
+++ components/GetPlaylist.tsx
@@ -1,169 +1,242 @@
-import { Button, Container, TextField } from '@mui/material'
-import React, { useCallback, useEffect, useState } from 'react'
-import { motion } from "framer-motion"
-import MenuNav from './MenuNav';
-
+import { Button, Container, TextField } from "@mui/material";
+import React, { useCallback, useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import MenuNav from "./MenuNav";
 
 type Props = {
-    setSongs: (songs: any[]) => void;
-    open: boolean;
-    setNextToken: (token: string | null) => void;
-    setCurrentPlaylistId: (id: string | null) => void;
-    handleOpen: () => void;
-    setLoading: (loading: boolean) => void;
-}
+  setSongs: (songs: any[]) => void;
+  open: boolean;
+  setNextToken: (token: string | null) => void;
+  setCurrentPlaylistId: (id: string | null) => void;
+  handleOpen: () => void;
+  setLoading: (loading: boolean) => void;
+};
 
-const GetPlaylist = ({ handleOpen, setSongs, open, setNextToken, setCurrentPlaylistId, setLoading }: Props) => {
+const GetPlaylist = ({
+  handleOpen,
+  setSongs,
+  open,
+  setNextToken,
+  setCurrentPlaylistId,
+  setLoading,
+}: Props) => {
+  const [url, setUrl] = useState("");
+  const [name, setName] = useState("");
+  const [inputLabel, setInputLabel] = useState("Playlist URL");
+  const [error, setError] = useState(false);
+  const [alignment, setAlignment] = useState("Add");
+  const [playlists, setPlaylists] = useState<any[]>([]);
 
-    const [url, setUrl] = useState('');
-    const [name, setName] = useState('');
-    const [inputLabel, setInputLabel] = useState('Playlist URL')
-    const [error, setError] = useState(false)
-    const [alignment, setAlignment] = useState('Add');
-    const [playlists, setPlaylists] = useState<any[]>([]);
+  const handleTab = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string
+  ) => {
+    if (newAlignment === "Add" || newAlignment === "My")
+      setAlignment(newAlignment);
+  };
 
-    const handleTab = (
-        event: React.MouseEvent<HTMLElement>,
-        newAlignment: string,
-    ) => {
-        if (newAlignment === 'Add' || newAlignment === 'My') setAlignment(newAlignment);
-    };
+  const displayError = () => {
+    setError(true);
+    setInputLabel("Invalid playlist URL");
+  };
 
-    const displayError = () => {
-        setError(true)
-        setInputLabel('Invalid playlist URL')
+  const getPlaylist = (url: string) => {
+    if (!url || !url.includes("list="))
+      return displayError(), setLoading(false);
+    const playlistId = url.split("list=")[1].split("&")[0];
+    fetch(`/api/playlist/?playlistId=${playlistId}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setNextToken(res.nextPageToken);
+        setCurrentPlaylistId(playlistId);
+        setSongs(
+          res.items.filter(
+            (item: { snippet: { title: string } }) =>
+              item.snippet.title !== "Deleted video"
+          )
+        );
+        setError(false);
+        setInputLabel("Playlist URL");
+        const playlists = JSON.parse(localStorage.getItem("playlists") || "[]");
+        setPlaylists(playlists);
+        if (playlists.some((playlist: { url: string }) => playlist.url === url))
+          return setLoading(false);
+        playlists.unshift({
+          name: name || "Playlist",
+          url: url,
+          totalResults: res.pageInfo.totalResults,
+        });
+        localStorage.setItem("playlists", JSON.stringify(playlists));
+      })
+      .then(() => {
+        if (open) handleOpen();
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setInputLabel("Invalid Playlist URL");
+        setLoading(false);
+      })
+      .finally(() => setLoading(false));
+  };
+
+  const handleRemove = (url: string) => {
+    const playlists = JSON.parse(localStorage.getItem("playlists") || "[]");
+    const newPlaylists = playlists.filter(
+      (playlist: { url: string }) => playlist.url !== url
+    );
+    localStorage.setItem("playlists", JSON.stringify(newPlaylists));
+    setPlaylists(newPlaylists);
+  };
+
+  useEffect(() => {
+    const playlists = JSON.parse(localStorage.getItem("playlists") || "[]");
+    if (playlists.length > 0) {
+      getPlaylist(playlists[0].url);
+    } else {
+      setLoading(false);
     }
+  }, []);
 
-    const getPlaylist = (url: string) => {
-        if (!url || !url.includes('list=')) return displayError(), setLoading(false);
-        const playlistId = url.split('list=')[1].split('&')[0];
-        fetch(`/api/playlist/?playlistId=${playlistId}`)
-            .then(res => res.json())
-            .then(res => {
-                setNextToken(res.nextPageToken)
-                setCurrentPlaylistId(playlistId)
-                setSongs(res.items.filter((item: { snippet: { title: string; }; }) => item.snippet.title !== 'Deleted video'))
-                setError(false)
-                setInputLabel('Playlist URL')
-                const playlists = JSON.parse(localStorage.getItem('playlists') || '[]')
-                setPlaylists(playlists)
-                if (playlists.some((playlist: { url: string; }) => playlist.url === url)) return setLoading(false);
-                playlists.unshift({
-                    name: name || 'Playlist',
-                    url: url,
-                    totalResults: res.pageInfo.totalResults,
-                })
-                localStorage.setItem('playlists', JSON.stringify(playlists))
-            }
-            )
-            .then(() => {
-                if (open) handleOpen()
-                setLoading(false)
-            })
-            .catch(() => {
-                setError(true)
-                setInputLabel('Invalid Playlist URL')
-                setLoading(false)
-            })
-            .finally(() => setLoading(false))
-    }
-
-    const handleRemove = (url: string) => {
-        const playlists = JSON.parse(localStorage.getItem('playlists') || '[]')
-        const newPlaylists = playlists.filter((playlist: { url: string; }) => playlist.url !== url)
-        localStorage.setItem('playlists', JSON.stringify(newPlaylists))
-        setPlaylists(newPlaylists)
-    }
-
-    useEffect(() => {
-        const playlists = JSON.parse(localStorage.getItem('playlists') || '[]')
-        if (playlists.length > 0) {
-            getPlaylist(playlists[0].url)
-        } else {
-            setLoading(false)
-        }
-
-    }
-        , [])
-
-
-    return (
-        <Container disableGutters sx={{ zIndex: 1 }}>
-            <motion.div initial={{ height: 0 }}
-                animate={{ overflow: 'hidden', height: open ? 'auto' : 0 }} >
-                <Container disableGutters sx={styles.container}>
-                    <MenuNav handleTab={handleTab} alignment={alignment} />
-                    {alignment === 'Add' && (
-                        <>
-                            <TextField fullWidth label='Playlist name' variant="outlined" onChange={(e) => setName(e.target.value)} />
-                            <TextField fullWidth label={inputLabel} variant="outlined" onChange={(e) => setUrl(e.target.value)} error={error} />
-                            <Container disableGutters sx={styles.buttons}>
-                                <Button variant='contained' color='error' sx={styles.button} onClick={handleOpen}>Close</Button>
-                                <Button variant='contained' color='success' sx={styles.button} onClick={() => getPlaylist(url)}>Save</Button>
-                            </Container>
-                        </>
-                    )}
-                    {alignment === 'My' && (
-                        <>
-                            {playlists.length === 0 ?
-                                <p style={{ textAlign: 'center' }}>No playlists yet..</p>
-                                : null}
-                            {playlists.map((playlist: {
-                                totalResults: number; url: string; name: string;
-                            }, index: number) => (
-                                <Container disableGutters key={index} sx={styles.playlist} className='background'>
-                                    <div>
-                                        <p>{index + 1}. {playlist.name}</p>
-                                        <p style={{ fontSize: '0.9em', color: 'gray' }}>{playlist.totalResults} songs</p>
-                                    </div>
-                                    <div style={{ display: 'flex', gap: 15 }}>
-                                        <Button variant='text' color='error' onClick={() => handleRemove(playlist.url)}>Remove</Button>
-                                        <Button variant='text' color='primary' onClick={() => getPlaylist(playlist.url)}>Open</Button>
-                                    </div>
-                                </Container>
-                            ))}
-                            <Button variant='contained' color='error' sx={styles.button} onClick={handleOpen}>Close</Button>
-                        </>
-                    )}
-                </Container>
-            </motion.div >
-        </Container >
-    )
-}
+  return (
+    <Container disableGutters sx={{ zIndex: 1 }}>
+      <motion.div
+        initial={{ height: 0 }}
+        animate={{ overflow: "hidden", height: open ? "auto" : 0 }}
+      >
+        <Container disableGutters sx={styles.container}>
+          <MenuNav handleTab={handleTab} alignment={alignment} />
+          {alignment === "Add" && (
+            <>
+              <TextField
+                fullWidth
+                label="Playlist name"
+                variant="outlined"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <TextField
+                fullWidth
+                label={inputLabel}
+                variant="outlined"
+                onChange={(e) => setUrl(e.target.value)}
+                error={error}
+              />
+              <Container disableGutters sx={styles.buttons}>
+                <Button
+                  variant="contained"
+                  color="error"
+                  sx={styles.button}
+                  onClick={handleOpen}
+                >
+                  Close
+                </Button>
+                <Button
+                  variant="contained"
+                  color="success"
+                  sx={styles.button}
+                  onClick={() => getPlaylist(url)}
+                >
+                  Save
+                </Button>
+              </Container>
+            </>
+          )}
+          {alignment === "My" && (
+            <>
+              {playlists.length === 0 ? (
+                <p style={{ textAlign: "center" }}>No playlists yet..</p>
+              ) : null}
+              {playlists.map(
+                (
+                  playlist: {
+                    totalResults: number;
+                    url: string;
+                    name: string;
+                  },
+                  index: number
+                ) => (
+                  <Container
+                    disableGutters
+                    key={index}
+                    sx={styles.playlist}
+                    className="background"
+                  >
+                    <div>
+                      <p>
+                        {index + 1}. {playlist.name}
+                      </p>
+                      <p style={{ fontSize: "0.9em", color: "gray" }}>
+                        {playlist.totalResults} songs
+                      </p>
+                    </div>
+                    <div style={{ display: "flex", gap: 15 }}>
+                      <Button
+                        variant="text"
+                        color="error"
+                        onClick={() => handleRemove(playlist.url)}
+                      >
+                        Remove
+                      </Button>
+                      <Button
+                        variant="text"
+                        color="primary"
+                        onClick={() => getPlaylist(playlist.url)}
+                      >
+                        Open
+                      </Button>
+                    </div>
+                  </Container>
+                )
+              )}
+              <Button
+                variant="contained"
+                color="error"
+                sx={styles.button}
+                onClick={handleOpen}
+              >
+                Close
+              </Button>
+            </>
+          )}
+        </Container>
+      </motion.div>
+    </Container>
+  );
+};
 
 const styles = {
-    button: {
-        display: 'block',
-        margin: '0 auto',
-        width: '20%',
-        p: 1,
-    },
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2,
-        marginTop: 2,
-        padding: 2,
-        overflow: 'hidden',
-        borderRadius: '5px',
-        backgroundColor: 'rgb(238, 238, 238)',
-        boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.1)',
-    },
-    buttons: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 2
-    },
-    playlist: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        gap: 2,
-        p: 2,
-        borderRadius: '5px',
-        backgroundColor: 'rgba(157, 157, 157, 0.25)',
-    }
-}
+  button: {
+    display: "block",
+    margin: "0 auto",
+    width: "20%",
+    p: 1,
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 2,
+    marginTop: 2,
+    padding: 2,
+    overflow: "hidden",
+    borderRadius: "5px",
+    backgroundColor: "rgb(238, 238, 238)",
+    boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.1)",
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 2,
+  },
+  playlist: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 2,
+    p: 2,
+    borderRadius: "5px",
+    backgroundColor: "rgba(157, 157, 157, 0.25)",
+  },
+};
 
-export default GetPlaylist
+export default GetPlaylist;
--- components/Player.tsx
+++ components/Player.tsx
@@ -1,155 +1,196 @@
-import { CircularProgress, Container, Typography } from '@mui/material';
-import dynamic from 'next/dynamic';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react'
-import NoPlaylistBox from './NoPlaylistBox';
-const ReactPlayer = dynamic(() => import("react-player/youtube"), { ssr: false });
+import { CircularProgress, Container, Typography } from "@mui/material";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import NoPlaylistBox from "./NoPlaylistBox";
+const ReactPlayer = dynamic(() => import("react-player/youtube"), {
+  ssr: false,
+});
 
 type Props = {
-    songs: any[]
-    nextToken: string | null
-    currentPlaylistId: string | null
-    setSongs: (songs: any[]) => void
-    setNextToken: (token: string | null) => void
-    loading: boolean
-}
+  songs: any[];
+  nextToken: string | null;
+  currentPlaylistId: string | null;
+  setSongs: (songs: any[]) => void;
+  setNextToken: (token: string | null) => void;
+  loading: boolean;
+};
 
-const Player = ({ songs, setSongs, setNextToken, nextToken, currentPlaylistId, loading }: Props) => {
-    const [currentSong, setCurrentSong] = useState(0);
-    const [currentUrl, setCurrentUrl] = useState('');
-    const [canUpdate, setCanUpdate] = useState(true);
+const Player = ({
+  songs,
+  setSongs,
+  setNextToken,
+  nextToken,
+  currentPlaylistId,
+  loading,
+}: Props) => {
+  const [currentSong, setCurrentSong] = useState(0);
+  const [currentUrl, setCurrentUrl] = useState("");
+  const [canUpdate, setCanUpdate] = useState(true);
 
-    const handleSong = (song: any, index: number) => {
-        setCurrentSong(index);
-        setCurrentUrl(getSongUrl(song));
-    }
+  const handleSong = (song: any, index: number) => {
+    setCurrentSong(index);
+    setCurrentUrl(getSongUrl(song));
+  };
 
-    const getSongUrl = (song: any) => {
-        if (!song) return '';
-        return `https://www.youtube.com/watch?v=${song.snippet.resourceId.videoId}`
-    }
+  const getSongUrl = (song: any) => {
+    if (!song) return "";
+    return `https://www.youtube.com/watch?v=${song.snippet.resourceId.videoId}`;
+  };
 
-    const handleLoadMore = async () => {
-        if (!nextToken) return setCanUpdate(true);
-        fetch(`/api/moreSongs/?playlistId=${currentPlaylistId}&pageToken=${nextToken}`)
-            .then(res => res.json())
-            .then(res => {
-                setSongs([
-                    ...songs,
-                    ...res.items.filter((item: { snippet: { title: string; }; }) => (
-                        item.snippet.title !== 'Deleted video' &&
-                        item.snippet.title !== 'Private video'
-                    ))
-                ]);
-                setNextToken(res.nextPageToken);
-            }
-            )
-            .catch(() => {
-                setNextToken(null);
-            }
-            )
-            .finally(() => setCanUpdate(true))
-    }
-
-    const handleScroll = (e: React.UIEvent) => {
-        if (!canUpdate) return;
-        if (e.currentTarget.scrollTop + e.currentTarget.clientHeight >= e.currentTarget.scrollHeight - 200 && canUpdate) {
-            setCanUpdate(false)
-            handleLoadMore();
-        }
-    }
-
-    useEffect(() => {
-        if (songs?.length > 0 && currentSong === 0) {
-            setCurrentUrl(getSongUrl(songs[0]));
-        }
-    }
-        , [currentSong, songs]);
-
-    return (
-        <Container disableGutters sx={styles.container}>
-            {songs?.length === 0 ? <NoPlaylistBox /> :
-                <>
-                    <Container disableGutters sx={{ height: { xs: '30vh', md: '50vh' } }} >
-                        <ReactPlayer
-                            fallback={<CircularProgress />}
-                            controls
-                            url={currentUrl}
-                            width='100%'
-                            height='100%'
-                            playing={true}
-                            onEnded={() => {
-                                setCurrentSong(currentSong + 1);
-                                setCurrentUrl(getSongUrl(songs[currentSong + 1]));
-                            }}
-                        />
-                    </Container>
-                    <Container disableGutters sx={styles.songsContainer} onScroll={handleScroll}>
-                        {songs?.map((song, index) => (
-                            <Container disableGutters sx={[styles.songCard, index === currentSong ? styles.active : null]} key={index} className='background noScrollbar' onClick={() => handleSong(song, index)}>
-                                {song?.snippet?.thumbnails?.high?.url ?
-                                    <Image src={song.snippet.thumbnails.high.url}
-                                        alt={song.snippet.title}
-                                        layout="fixed"
-                                        width={200}
-                                        height={90}
-                                        style={{ borderTopLeftRadius: 5, borderBottomLeftRadius: 5 }}
-                                    /> : null}
-                                <p style={{ wordBreak: 'break-word', textAlign: 'left', width: '100%', padding: '0 5px' }}>{song.snippet?.title}</p>
-                            </Container>
-                        ))}
-                    </Container>
-                </>
-            }
-        </Container>
+  const handleLoadMore = async () => {
+    if (!nextToken) return setCanUpdate(true);
+    fetch(
+      `/api/moreSongs/?playlistId=${currentPlaylistId}&pageToken=${nextToken}`
     )
-}
+      .then((res) => res.json())
+      .then((res) => {
+        setSongs([
+          ...songs,
+          ...res.items.filter(
+            (item: { snippet: { title: string } }) =>
+              item.snippet.title !== "Deleted video" &&
+              item.snippet.title !== "Private video"
+          ),
+        ]);
+        setNextToken(res.nextPageToken);
+      })
+      .catch(() => {
+        setNextToken(null);
+      })
+      .finally(() => setCanUpdate(true));
+  };
+
+  const handleScroll = (e: React.UIEvent) => {
+    if (!canUpdate) return;
+    if (
+      e.currentTarget.scrollTop + e.currentTarget.clientHeight >=
+        e.currentTarget.scrollHeight - 200 &&
+      canUpdate
+    ) {
+      setCanUpdate(false);
+      handleLoadMore();
+    }
+  };
+
+  useEffect(() => {
+    if (songs?.length > 0 && currentSong === 0) {
+      setCurrentUrl(getSongUrl(songs[0]));
+    }
+  }, [currentSong, songs]);
+
+  return (
+    <Container disableGutters sx={styles.container}>
+      {songs?.length === 0 ? (
+        <NoPlaylistBox />
+      ) : (
+        <>
+          <Container disableGutters sx={{ height: { xs: "30vh", md: "50vh" } }}>
+            <ReactPlayer
+              fallback={<CircularProgress />}
+              controls
+              url={currentUrl}
+              width="100%"
+              height="100%"
+              playing={true}
+              stopOnUnmount={false}
+              onEnded={() => {
+                setCurrentSong(currentSong + 1);
+                setCurrentUrl(getSongUrl(songs[currentSong + 1]));
+              }}
+            />
+          </Container>
+          <Container
+            disableGutters
+            sx={styles.songsContainer}
+            onScroll={handleScroll}
+          >
+            {songs?.map((song, index) => (
+              <Container
+                disableGutters
+                sx={[
+                  styles.songCard,
+                  getSongUrl(song) === currentUrl ? styles.active : null,
+                ]}
+                key={index}
+                className="background noScrollbar"
+                onClick={() => handleSong(song, index)}
+              >
+                {song?.snippet?.thumbnails?.high?.url ? (
+                  <Image
+                    src={song.snippet.thumbnails.high.url}
+                    alt={song.snippet.title}
+                    layout="fixed"
+                    width={200}
+                    height={90}
+                    style={{
+                      borderTopLeftRadius: 5,
+                      borderBottomLeftRadius: 5,
+                    }}
+                  />
+                ) : null}
+                <p
+                  style={{
+                    wordBreak: "break-word",
+                    textAlign: "left",
+                    width: "100%",
+                    padding: "0 5px",
+                  }}
+                >
+                  {song.snippet?.title}
+                </p>
+              </Container>
+            ))}
+          </Container>
+        </>
+      )}
+    </Container>
+  );
+};
 
 const styles = {
-    container: {
-        mt: { xs: 2, md: 5 },
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'space-between',
-        flexDirection: { xs: 'column', md: 'row' },
-        gap: { xs: 0, md: 2 },
+  container: {
+    mt: { xs: 2, md: 5 },
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "space-between",
+    flexDirection: { xs: "column", md: "row" },
+    gap: { xs: 0, md: 2 },
+  },
+  songsContainer: {
+    mt: { xs: 2, md: 0 },
+    overflowY: "scroll",
+    scrollBehavior: "smooth",
+    height: { xs: "calc(70vh - 140px)", md: "50vh" },
+    // hide scrollbar all browsers
+    scrollBarWith: "none",
+    msOverflowStyle: "none",
+    "&::-webkit-scrollbar": {
+      display: "none",
+      width: 0,
+      height: 0,
     },
-    songsContainer: {
-        mt: { xs: 2, md: 0 },
-        overflowY: 'scroll',
-        scrollBehavior: 'smooth',
-        height: { xs: 'calc(70vh - 140px)', md: '50vh' },
-        // hide scrollbar all browsers
-        scrollBarWith: 'none',
-        msOverflowStyle: 'none',
-        '&::-webkit-scrollbar': {
-            display: 'none',
-            width: 0,
-            height: 0
-        }
-
-
+  },
+  songCard: {
+    color: "white",
+    display: "flex",
+    justifyContent: "flex-start",
+    gap: 2,
+    width: "100%",
+    borderRadius: "5px",
+    backgroundColor: "rgba(157, 157, 157, 0.25)",
+    mb: 1.2,
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "rgba(255, 255, 255, 0.3)",
     },
-    songCard: {
-        color: 'white',
-        display: 'flex',
-        justifyContent: 'flex-start',
-        gap: 2,
-        width: '100%',
-        borderRadius: '5px',
-        backgroundColor: 'rgba(157, 157, 157, 0.25)',
-        mb: 1.2,
-        cursor: 'pointer',
-        '&:hover': {
-            backgroundColor: 'rgba(255, 255, 255, 0.3)',
-        },
+  },
+  active: {
+    backgroundColor: "rgba(20, 162, 0, 0.4)",
+    "&:hover": {
+      backgroundColor: "rgba(20, 162, 0, 0.6)",
     },
-    active: {
-        backgroundColor: 'rgba(20, 162, 0, 0.4)',
-        '&:hover': {
-            backgroundColor: 'rgba(20, 162, 0, 0.6)'
-        },
-    }
-}
+  },
+};
 
-export default Player
+export default Player;
